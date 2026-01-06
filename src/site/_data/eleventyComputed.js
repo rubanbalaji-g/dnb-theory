8773e5796cb553667e00f39d9496ce398199e9b8
rubanbalaji-g <115378336+rubanbalaji-g@users.noreply.github.com>
2026-01-07 00:47:45 +0530

    Update eleventyComputed.js

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -3,7 +3,8 @@ const { getFileTree } = require("../../helpers/filetreeUtils");
 const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
+  // These must all be async and use await
   graph: async (data) => await getGraph(data),
-  filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data)
+  filetree: async (data) => await getFileTree(data),
+  userComputed: async (data) => await userComputed(data)
 };
